1eb3d27e33ca36a3428c61faf91a70276607e935
synzen <michael994664@gmail.com>
2021-04-04 15:23:43 -0400

    Export RESTProducer, RESTConsumer

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import RESTHandler from "./RESTHandler"
 import APIRequest from './APIRequest'
+import RESTProducer from './RESTProducer'
+import RESTConsumer from './RESTConsumer'
 
 export {
   RESTHandler,
-  APIRequest
+  APIRequest,
+  RESTProducer,
+  RESTConsumer
 }
